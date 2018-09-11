5a910eefc44d7d7a98d78dd375fd7d585154acbd
Lukasz Byczynski <l.byczynski@adform.com>
2018-09-11 11:25:16 +0200

    Pure IO version

--- cats-effect/src/main/scala/fp/testfield/cats/effect/TryToFetch100ItemsApp.scala
+++ cats-effect/src/main/scala/fp/testfield/cats/effect/TryToFetch100ItemsApp.scala
@@ -12,12 +12,17 @@ object TryToFetch100ItemsApp extends IOApp {
   import cats._
   import cats.implicits._
 
-  def randomIO[F[_]](implicit F: Sync[F]): F[String] = F.delay {
-    println("[START]: random IO")
-    val start = System.currentTimeMillis()
-    Thread.sleep(500 + Random.nextInt(600))
-    println(s"[STOP]: took ${ System.currentTimeMillis() - start } ms")
-    "ok"
+  def randomIO[F[_] : Monad](implicit F: Timer[F], S : Sync[F]): F[String] = {
+    for {
+      _ <- S.delay {
+        println("[START]: random IO")
+      }
+      startTime <- S.delay(System.currentTimeMillis())
+      _ <- F.sleep((500 + Random.nextInt(600)).millis)
+      _ <- S.delay {
+        println(s"[STOP]: took ${System.currentTimeMillis() - startTime} ms")
+      }
+    } yield "ok"
   }
 
   /*_*/
@@ -31,9 +36,20 @@ object TryToFetch100ItemsApp extends IOApp {
 
   override def run(args: List[String]): IO[ExitCode] = {
 
-    val tasks = List.fill(100)(randomIO[IO].timeout(1.second).attempt)
+    val tasks = List.fill(100)(randomIO[IO].timeout(950.millis).attempt)
 
-    val boostrap = customThreadPool[IO].use { ec => tasks.parTraverse(e => contextShift.evalOn(ec)(e).start)
+    val program = for {
+      startTime <- IO.delay(System.currentTimeMillis())
+      fibers <- tasks.traverse(_.start)
+      result <- fibers.traverse(_.join)
+      _ <- IO.delay {
+        println(s"[APP]: took ${System.currentTimeMillis() - startTime} ms")
+      }
+    } yield result
+
+    program.timeout(1.second).map { r =>
+      println(r)
+      ExitCode.Success
     }
   }
 
