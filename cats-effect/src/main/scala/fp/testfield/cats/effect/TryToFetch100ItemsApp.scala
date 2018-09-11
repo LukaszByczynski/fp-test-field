6d46917732cb5a52206a9444d4cea93b4dbde090
Lukasz Byczynski <l.byczynski@adform.com>
2018-09-11 13:22:15 +0200

    Solution for TryToFetch100ItemsApp

--- cats-effect/src/main/scala/fp/testfield/cats/effect/TryToFetch100ItemsApp.scala
+++ cats-effect/src/main/scala/fp/testfield/cats/effect/TryToFetch100ItemsApp.scala
@@ -9,46 +9,37 @@ import scala.concurrent.duration._
 import scala.util.Random
 
 object TryToFetch100ItemsApp extends IOApp {
-  import cats._
+
   import cats.implicits._
 
-  def randomIO[F[_] : Monad](implicit F: Timer[F], S : Sync[F]): F[String] = {
+  val executor = Executors.newFixedThreadPool(8)
+  val ec       = ExecutionContext.fromExecutor(executor)
+
+  def randomIo: IO[String] =
     for {
-      _ <- S.delay {
-        println("[START]: random IO")
-      }
-      startTime <- S.delay(System.currentTimeMillis())
-      _ <- F.sleep((500 + Random.nextInt(600)).millis)
-      _ <- S.delay {
-        println(s"[STOP]: took ${System.currentTimeMillis() - startTime} ms")
-      }
+      _         <- IO.shift(ec)
+      _         <- IO.delay(println("[START]: random IO"))
+      startTime <- IO.delay(System.currentTimeMillis())
+      _         <- IO(Thread.sleep(500 + Random.nextInt(600)))
+      _         <- IO.delay(println(s"[STOP]: took ${System.currentTimeMillis() - startTime} ms"))
     } yield "ok"
-  }
-
-  /*_*/
-  def customThreadPool[F[_]](implicit F: Sync[F]): Resource[F, ExecutionContext] =
-    Resource(F.delay {
-      val executor = Executors.newFixedThreadPool(5)
-      val ec = ExecutionContext.fromExecutor(executor)
-      (ec, F.delay(executor.shutdown()))
-    })
-
 
   override def run(args: List[String]): IO[ExitCode] = {
-
-    val tasks = List.fill(100)(randomIO[IO].timeout(950.millis).attempt)
-
     val program = for {
       startTime <- IO.delay(System.currentTimeMillis())
-      fibers <- tasks.traverse(_.start)
-      result <- fibers.traverse(_.join)
+      tasks     <- IO(List.fill(100)(randomIo.timeout(1.second).attempt))
+      fibers    <- tasks.parTraverse(_.start)
+      result    <- fibers.parTraverse(_.join)
       _ <- IO.delay {
         println(s"[APP]: took ${System.currentTimeMillis() - startTime} ms")
       }
     } yield result
 
-    program.timeout(1.second).map { r =>
+    program.timeout(2.second).map { r =>
       println(r)
+      println(s"Right: ${r.count(_.isRight)}")
+      println(s"Left: ${r.count(_.isLeft)}")
+      executor.shutdown()
       ExitCode.Success
     }
   }
