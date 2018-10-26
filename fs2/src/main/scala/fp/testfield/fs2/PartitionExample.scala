9d116cc9b7afa0f1ec1a08b11bd0ff584da59389
Lukasz Byczynski <l.byczynski@adform.com>
2018-10-26 12:13:12 +0200

    Change Grouping example to partition (brand new pipe code)

---
+++ fs2/src/main/scala/fp/testfield/fs2/PartitionExample.scala
@@ -0,0 +1,88 @@
+package fp.testfield.fs2
+
+import cats.effect._
+import cats.implicits._
+
+final case class Record(tag: String, data: Array[Int])
+
+final class Exporter(val tag: String) {
+  println(s"new exporter $tag")
+
+  def close(): Unit = {
+    println(s"close exporter $tag")
+  }
+
+  def write(record: Record): Unit = {
+    println(s"w $tag")
+  }
+}
+
+final class FileOps[F[_]](val name: String)(implicit S: Sync[F]) {
+  println(s"Open file: $name")
+
+  def close(): F[Unit] = {
+    S.delay(println(s"close file $name"))
+  }
+}
+
+class PartitionPipe[F[_]](implicit S: Sync[F]) {
+
+  private def isTagChange(state: Option[Exporter], tag: String): Boolean = {
+    state.isEmpty || state.exists(_.tag != tag)
+  }
+
+  /*_*/
+  private def go(inputStream: fs2.Stream[F, Record], exporter: Option[Exporter]): fs2.Pull[F, String, Unit] = {
+    val tryPushResultAndReleaseExporter = exporter match {
+      case Some(st) =>
+        val pushResult = fs2.Pull
+          .acquireCancellable(S.delay(new FileOps[F](st.tag)))(_.close())
+          .flatMap(file => fs2.Pull.output1(s"${file.resource.name}.pq").onComplete(file.cancel))
+
+        pushResult >> fs2.Pull.eval(S.delay(exporter.foreach(_.close())))
+      case _ =>
+        fs2.Pull.pure(())
+    }
+
+    inputStream.pull.uncons1.flatMap {
+      case None => tryPushResultAndReleaseExporter >> fs2.Pull.done
+      case Some((record, tailStream)) =>
+        val writeRecord = (state: Option[Exporter]) => fs2.Pull.eval(S.delay(state.foreach(_.write(record))))
+
+        if (isTagChange(exporter, record.tag)) {
+          val createExporter = fs2.Pull.eval(S.delay(Some(new Exporter(record.tag))))
+
+          tryPushResultAndReleaseExporter >> createExporter.flatMap { newExporter =>
+            writeRecord(newExporter) >> go(tailStream, newExporter)
+          }
+        } else {
+          writeRecord(exporter) >> go(tailStream, exporter)
+        }
+    }
+  }
+
+  def pipe: fs2.Pipe[F, Record, String] = in => go(in, None).streamNoScope
+}
+
+object PartitionExample extends IOApp {
+
+  /*_*/
+  override def run(args: List[String]): IO[ExitCode] = {
+    fs2.Stream
+      .range(1, 10)
+      .map(createRecord)
+      .covary[IO]
+      .through(new PartitionPipe[IO].pipe)
+      .evalMap(w => IO.delay(println(s"hdfs $w")))
+      .compile
+      .drain
+      .map(_ => ExitCode.Success)
+  }
+
+  private def createRecord(i: Int): Record = i match {
+    case v if v < 5 => Record("a", Array.fill(1024)(0))
+    case v if v < 7 => Record("b", Array.fill(1024)(0))
+    case v if v == 8 => throw new IllegalArgumentException("a")
+    case _ => Record("c", Array.fill(1024)(0))
+  }
+}
