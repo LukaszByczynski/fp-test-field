4be9bb883177b09cb1098c9d7633afe8d72199f9
Lukasz Byczynski <l.byczynski@adform.com>
2018-10-30 14:58:18 +0100

    Debug msg

--- fs2/src/main/scala/fp/testfield/fs2/PartitionExample.scala
+++ fs2/src/main/scala/fp/testfield/fs2/PartitionExample.scala
@@ -2,26 +2,27 @@ package fp.testfield.fs2
 
 import cats.effect._
 import cats.implicits._
+import fs2.Pull
 
 final case class Record(tag: String, data: Array[Int])
 
 final class Exporter(val tag: String) {
-  println(s"new exporter $tag")
+  println(s"${System.nanoTime()} new exporter $tag")
 
   def close(): Unit = {
-    println(s"close exporter $tag")
+    println(s"${System.nanoTime()} close exporter $tag")
   }
 
   def write(record: Record): Unit = {
-    println(s"w $tag")
+    println(s"${System.nanoTime()} w $tag")
   }
 }
 
 final class FileOps[F[_]](val name: String)(implicit S: Sync[F]) {
-  println(s"Open file: $name")
+  println(s"${System.nanoTime()} Open file: $name")
 
   def close(): F[Unit] = {
-    S.delay(println(s"close file $name"))
+    S.delay(println(s"${System.nanoTime()} close file $name"))
   }
 }
 
@@ -33,13 +34,15 @@ class PartitionPipe[F[_]](implicit S: Sync[F]) {
 
   /*_*/
   private def go(inputStream: fs2.Stream[F, Record], exporter: Option[Exporter]): fs2.Pull[F, String, Unit] = {
-    val tryPushResultAndReleaseExporter = exporter match {
+    def closeExporter = fs2.Pull.eval(S.delay(exporter.foreach(_.close())))
+
+    def tryPushResultAndReleaseExporter = exporter match {
       case Some(st) =>
         val pushResult = fs2.Pull
           .acquireCancellable(S.delay(new FileOps[F](st.tag)))(_.close())
           .flatMap(file => fs2.Pull.output1(s"${file.resource.name}.pq").onComplete(file.cancel))
 
-        pushResult >> fs2.Pull.eval(S.delay(exporter.foreach(_.close())))
+        closeExporter >> pushResult
       case _ =>
         fs2.Pull.pure(())
     }
@@ -47,12 +50,12 @@ class PartitionPipe[F[_]](implicit S: Sync[F]) {
     inputStream.pull.uncons1.flatMap {
       case None => tryPushResultAndReleaseExporter >> fs2.Pull.done
       case Some((record, tailStream)) =>
-        val writeRecord = (state: Option[Exporter]) => fs2.Pull.eval(S.delay(state.foreach(_.write(record))))
+        def writeRecord = (state: Option[Exporter]) => fs2.Pull.eval(S.delay(state.foreach(_.write(record))))
 
         if (isTagChange(exporter, record.tag)) {
-          val createExporter = fs2.Pull.eval(S.delay(Some(new Exporter(record.tag))))
+          val createExporter = fs2.Pull.acquire(S.delay(Some(new Exporter(record.tag))))(st => S.delay(st.foreach(_.close())))
 
-          tryPushResultAndReleaseExporter >> createExporter.flatMap { newExporter =>
+          tryPushResultAndReleaseExporter >  createExporter.flatMap { newExporter =>
             writeRecord(newExporter) >> go(tailStream, newExporter)
           }
         } else {
@@ -73,7 +76,7 @@ object PartitionExample extends IOApp {
       .map(createRecord)
       .covary[IO]
       .through(new PartitionPipe[IO].pipe)
-      .evalMap(w => IO.delay(println(s"hdfs $w")))
+      .evalMap(w => IO.delay(println(s"${System.nanoTime()}  hdfs $w")))
       .compile
       .drain
       .map(_ => ExitCode.Success)
