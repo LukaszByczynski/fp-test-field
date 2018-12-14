843d704e3ee4fb8b10c6beae974bf76350c7d20e
Lukasz Byczynski <l.byczynski@adform.com>
2018-12-14 14:28:54 +0100

    Stage kind projection example

---
+++ cats-effect/src/main/scala/fp/testfield/KindProjectorExample.scala
@@ -0,0 +1,20 @@
+package fp.testfield
+
+import cats.implicits._
+import cats.Foldable
+import scala.util.Try
+
+object KindProjectorExample extends App {
+
+//  {type λ[α] = Either[String, α]})#W
+
+//  type E = ({type λ[α] = Either[String, α]})#λ
+
+  def foldIt[F[_] : Foldable](input: F[String]) =
+    input.foldM[({type λ[α] = Either[Int, α]})#λ  , Throwable](new Throwable) { case (_, s) => Try(s.toInt).toEither.swap }
+
+  println(foldIt(List("d", "u", "p", "7", "g")))
+  println(foldIt(Vector("d", "u", "p", "a")))
+  println(foldIt(List.empty[String]))
+
+}
--- cats-effect/src/main/scala/fp/testfield/effect/SleepSortExample.scala
+++ cats-effect/src/main/scala/fp/testfield/effect/SleepSortExample.scala
@@ -1,22 +1,22 @@
-package fp.testfield.fs2
+package fp.testfield.effect
 
-import cats._
+import cats.Monad
 import cats.effect._
-import cats.effect.concurrent.Ref
 import cats.implicits._
+import cats.effect.concurrent.Ref
 import cats.mtl.MonadState
 
 import scala.concurrent.duration._
 
-object SleepSort extends IOApp {
+object SleepSortExample extends IOApp {
 
   def sleepSort[F[_] : Monad](input: List[Int])(
     implicit MS: MonadState[F, List[Int]], T: Timer[F], C: Concurrent[F]
   ): F[List[Int]] = {
     input.traverse { i =>
       val fiber = for {
-        _ <- T.sleep(i.seconds)
-        _ <- C.delay(println(i))
+        _ <- T.sleep(i.second)
+//        _ <- C.delay(println(i))
         _ <- MS.modify(_ :+ i)
       } yield i
 
@@ -26,14 +26,16 @@ object SleepSort extends IOApp {
 
   /*_*/
   override def run(args: List[Predef.String]): IO[ExitCode] = {
+
     import com.olegpy.meow.effects._
+
+    val input:List[Int] = (0 to 100).map(_ => List(1, 5, 2, 9)).toList.flatten
 
     val sortedList = for {
       stateRef <- Ref[IO].of(List[Int]())
-      result <- stateRef.runState(implicit st => sleepSort[IO](List(1, 5, 2, 9)))
+      result <- stateRef.runState(implicit st => sleepSort[IO](input))
     } yield result
 
     sortedList.flatMap(r => IO.delay(println(r))) >> IO.delay(ExitCode.Success)
   }
 }
-
