c7b43e006f2f4fdee7bd811340c277cc8b6b3a74
Lukasz Byczynski <l.byczynski@adform.com>
2019-04-10 23:12:19 +0200

    ZIO Bv app

--- scalaz/src/main/scala/ex03_bv.scala
+++ scalaz/src/main/scala/ex03_bv.scala
@@ -7,7 +7,7 @@ import zio.interop.scalaz72._
 package repository {
 
   trait Repository[Id, T] {
-    val service: Repository.Service[Id, T]
+    val repositoryService: Repository.Service[Id, T]
   }
 
   object Repository {
@@ -18,41 +18,82 @@ package repository {
 
     }
   }
+}
 
-  object RepositoryInMem {
+package audit {
 
-    def apply[Id, T] = Ref.make(Map[Id, T]()).map { safeMap =>
-      new Repository[Id, T] {
-        override val service = new Repository.Service[Id, T] {
-          override def find(id: Id): Task[Option[T]] = {
-            safeMap.get.map(_.get(id))
-          }
+  trait Audit {
+    val auditService: Audit.Service
+  }
 
-          override def save(id: Id, item: T): Task[Unit] = {
-            safeMap.update(_ + (id -> item)) >> Task.unit
-          }
+  object Audit {
+
+    trait Service {
+      def audit(msg: String): Task[Unit]
+    }
+  }
+}
+
+object RepositoryInMem {
+
+  def apply[Id, T] = Ref.make(Map[Id, T]()).map { safeMap =>
+    new repository.Repository[Id, T] {
+      override val repositoryService = new repository.Repository.Service[Id, T] {
+        override def find(id: Id): Task[Option[T]] = {
+          safeMap.get.map(_.get(id))
+        }
+
+        override def save(id: Id, item: T): Task[Unit] = {
+          safeMap.update(_ + (id -> item)) >> Task.unit
         }
       }
     }
   }
 }
 
-object database {
-  import repository.Repository
+object auditor {
 
-  def find[Id, T](id: Id): ZIO[Repository[Id, T], Throwable, Option[T]] = {
-    ZIO.accessM(_.service.find(id))
+  def apply() = new audit.Audit {
+    override val auditService = new audit.Audit.Service() {
+      override def audit(msg: String): Task[Unit] = {
+        Task(println(s"auditor: $msg"))
+      }
+    }
+  }
+}
+
+object programs {
+  import audit._
+  import repository._
+
+  type Env         = Repository[Int, String] with Audit
+  type AppStack[A] = ZIO[Env, Throwable, A]
+
+  def findWithAudit(id: Int): AppStack[Option[String]] = {
+    for {
+      _   <- ZIO.accessM[Env](_.auditService.audit("query to db"))
+      res <- ZIO.accessM[Env](_.repositoryService.find(id))
+      _   <- ZIO.accessM[Env](_.auditService.audit(s"result $res"))
+    } yield res
   }
 }
 
 /*_*/
 object ex03_bv extends App {
+
+  import audit._
   import repository._
 
   override def run(args: List[String]) = {
     val program = RepositoryInMem[Int, String].flatMap { repo =>
+      val audit = auditor()
+      val rtm = new Repository[Int, String] with Audit {
+        override val repositoryService = repo.repositoryService
+        override val auditService      = audit.auditService
+      }
+
       for {
-        r <- database.find(2).provide(repo)
+        r <- programs.findWithAudit(2).provide(rtm)
         // _ <- repo.repository.save(2, "aaa")
         // r <- repo.repository.find(2)
         _ <- Task(println(r))
